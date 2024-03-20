57f47019eddf7a255c5568cd2c348e5f48905aad
sayoo789 <sayoojyapm2023@cs.ajce.in>
2024-03-20 16:43:19 +0530

    angular components example

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProductComponent } from './product/product.component';
+import { ContactComponent } from './contact/contact.component';
+
+
+const routes: Routes = [
+{
+  path:'',
+  component:HomeComponent
+},
+{
+  path:'about',
+  component:AboutComponent
+},
+{
+  path:'contact',
+  component:ContactComponent
+},
+{
+  path:'product',
+  component:ProductComponent
+}
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
